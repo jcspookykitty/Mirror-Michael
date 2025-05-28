bf47fea9c6811ecb08804148c9ca4ce2bf4e30c1
jcspookykitty <jcspookykitty@gmail.com>
2025-05-28 18:20:37 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const speakRoute = require('./routes/speak');
-const app = express();
-
-app.use(express.json());
-app.use('/speak', speakRoute);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Mirror Michael listening on port ${PORT}`));
