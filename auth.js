be9dd1049575fa7060a7311bd5e9446a21756eaa
jcspookykitty <jcspookykitty@gmail.com>
2025-05-29 13:58:08 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,17 +1,13 @@
-// auth.js
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_REDIRECT_URI
-},
-function(accessToken, refreshToken, profile, done) {
-  // Save user info to session or database here if needed
+}, function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }));
 
-// Serialize user into session
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
